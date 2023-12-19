17823cd979e2cd4b206ea8f72b0f0e8dd3b8eebf
xxjcaxx <xxjcaxx@yahoo.es>
2023-12-19 15:07:04 +0100

    ejercicios de fetch

--- spec/http_spec.mjs
+++ spec/http_spec.mjs
@@ -1,8 +1,11 @@
 import * as http from '../src/comunicacion_servidor.js'
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
 describe('Comunicación con el servidor', function() {
     describe('fetch', function() {
       it('getData debe obtener los datos o un error' , async function() {
+
         let promise = http.getData('__spec__/src/data/liga.json')
         expect(promise).toBeInstanceOf(Promise);
         try {
@@ -11,7 +14,48 @@ describe('Comunicación con el servidor', function() {
         } catch (error) {
             console.log(error);
         }
+        let promiseErrorRed = http.getData('http://dominioquenoexiste.noexiste/__spec__/src/data/liga.json')
+        expect(promiseErrorRed).toBeInstanceOf(Promise);
+        try {
+            let data = await promiseErrorRed;
+           // expect(data).toBeInstanceOf(Array);
+        } catch (error) {
+            expect(error).toBe("Error de red");
+        }
+        let promiseErrorServidor = http.getData('__spec__/src/data/noexiste.json')
+        expect(promiseErrorServidor).toBeInstanceOf(Promise);
+        try {
+            let data = await promiseErrorServidor;
+           // expect(data).toBeInstanceOf(Array);
+        } catch (error) {
+            expect(error).toBe("Error en el servidor");
+        }
+
+        let promiseErrorJSON= http.getData('__spec__/src/data/jsonMalo.json')
+        expect(promiseErrorJSON).toBeInstanceOf(Promise);
+        try {
+            let data = await promiseErrorJSON;
+           // expect(data).toBeInstanceOf(Array);
+        } catch (error) {
+            expect(error).toBe("El JSON no es correcto");
+        }
        
+      });
+
+      it('getDataCache debe obtener los datos en cache o del servidor' , async function() {
+        const spyFetch = spyOn(window, 'fetch').and.callThrough();;
+        let getLiga = http.getDataCache('__spec__/src/data/liga.json');
+        let promise = getLiga();
+        expect(promise).toBeInstanceOf(Promise);
+        let data = await promise;
+        expect(data).toBeInstanceOf(Array);
+        expect(spyFetch).toHaveBeenCalled();
+        // Cache
+        let promiseCache = getLiga();
+        let dataCache = await promiseCache;
+        expect(dataCache).toBeInstanceOf(Array);
+        expect(spyFetch).toHaveBeenCalledTimes(1);
+
       });
     });
    });
--- src/comunicacion_servidor.js
+++ src/comunicacion_servidor.js
@@ -1,22 +1,21 @@
-export {getData}
+export {getData, getDataCache}
 
 async function getData(url){
     /*
     Debe retornar los datos del servidor
-    En caso de error con la red, lanzará un mensaje indicando el error
-    En caso de error del servidor, 500 o 400, lanzará otro error
-    Los datos originales están en JSON, también tratará el posible error de parse
+    En caso de error con la red, lanzará un error con un mensaje personalizado "Error de red"
+    En caso de error del servidor, 500 o 400, lanzará otro error "Error en el servidor"
+    Los datos originales están en JSON, también tratará el posible error de parse "El JSON no es correcto"
     Los datos retornados serán en forma de Array o Objeto resultado de parsear el JSON original 
     */
-   try {
-     let response = await fetch(url);
-     if(response.status >= 200 && response.status < 300){
-        let data = await response.json();
-        return data;
-     } else {
-        throw response.statusText;
-     }
-   } catch (error) {
-    throw error;
-   }
 }
+
+function getDataCache(url){
+    /*
+    Debe retornar otra función para obtener los datos de la url indicada en esta función.
+    La función retornada, al ser invocada, retornará la promesa de descargar los datos.
+    Hay que implementar una cache, de manera que si se vuelve a invocar la función retornada, retorne lo de la cache.
+    Se puede hacer con una closure.
+    */
+}
+
