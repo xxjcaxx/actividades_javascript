92c54438e679b1c73e7722fb6dce605aaf74d3c5
xxjcaxx <xxjcaxx@yahoo.es>
2024-02-21 23:31:51 +0100

    inicio de algoritmos

---
+++ spec/algorithms_spec.mjs
@@ -0,0 +1,142 @@
+import * as alg from "../src/algoritmos.js"
+
+const initial_state = [[0,0,0],[0,0,0],[0,0,0]];
+const example_state = [[0,1,0],[0,-1,0],[0,0,0]];
+const full_state = [[-1,1,-1],[1,-1,1],[1,-1,1]];
+const win_state = [[-1,1,-1],[1,-1,1],[-1,-1,1]];
+
+describe('3 en raya', function() {
+    describe('Funciones del juego', function() {
+      it('TicTacToeGetInitialState Debe retornar un array 3x3 de 0s', function() {
+        expect(alg.TicTacToeGetInitialState()).toEqual(initial_state)
+        });
+      it('TicTacToeGetNextState Debe retornar una copia con la jugada', function() {
+        expect(alg.TicTacToeGetNextState(initial_state,2,1)).toEqual([[0,0,1],[0,0,0],[0,0,0]]);
+        expect(alg.TicTacToeGetNextState(initial_state,3,-1)).toEqual([[0,0,0],[-1,0,0],[0,0,0]]);
+        expect(alg.TicTacToeGetNextState(initial_state,2,1)).not.toBe(initial_state);
+        });
+      it('TicTacToeGetValidMoves Debe retornar la lista de posibles accions', function() {
+          expect(alg.TicTacToeGetValidMoves(initial_state)).toEqual([1,1,1,1,1,1,1,1,1]);
+          expect(alg.TicTacToeGetValidMoves(example_state)).toEqual([1,0,1,1,0,1,1,1,1]);
+          expect(alg.TicTacToeGetValidMoves(full_state)).toEqual([0,0,0,0,0,0,0,0,0]);
+        });
+
+        it('TicTacToeCheckWin Debe retornar si ha ganado o no el jugador', function() {
+          expect(alg.TicTacToeCheckWin(example_state,1)).toBe(false);
+          expect(alg.TicTacToeCheckWin(full_state,1)).toBe(false);
+          expect(alg.TicTacToeCheckWin(full_state,2)).toBe(false);
+          expect(alg.TicTacToeCheckWin(win_state,4)).toBe(true);
+          expect(alg.TicTacToeCheckWin(win_state,1)).toBe(false);
+        });
+
+        it('TicTacToeGetWinAndTerminated Debe retornar si ha ganado o no el jugador y si la partida a acabado', function() {
+          expect(alg.TicTacToeGetWinAndTerminated(example_state,1)).toEqual({win:false,terminated: false});
+          expect(alg.TicTacToeGetWinAndTerminated(full_state,1)).toEqual({win:false,terminated: true});
+          expect(alg.TicTacToeGetWinAndTerminated(win_state,4)).toEqual({win:true,terminated: true});
+        });
+        it('TicTacToeChangePerspective Debe retornar el juego invertido', function() {
+          //console.log(alg.TicTacToeChangePerspective(example_state));
+          expect(alg.TicTacToeChangePerspective(example_state)).toEqual([[0,-1,0],[0,1,0],[0,0,0]]);
+           });
+    });
+    describe('Juego completo', function() {
+      it('Debe completar un juego completo', function() {
+      const move_list = [4,0,1,7,2,8,6]
+      let state = alg.TicTacToeGetInitialState();
+      let player = 1;
+      for (let move of move_list){
+        state = alg.TicTacToeGetNextState(state,move,player);
+        player = alg.TicTacToeChangePlayer(player);
+      }
+      expect(state).toEqual([[-1,1,1],[0,1,0],[1,-1,-1]]);
+      expect(alg.TicTacToeCheckWin(state,move_list.at(-1))).toBe(true);
+      expect(alg.TicTacToeGetWinAndTerminated(state,move_list.at(-1))).toEqual({win:true, terminated: true});
+      });
+    });
+   });
+
+
+describe('Montecarlo', function() {
+  const game = {
+    getValidMoves: alg.TicTacToeGetValidMoves,
+    getInitialState: alg.TicTacToeGetInitialState,
+    getNextState: alg.TicTacToeGetNextState,
+    changePerspective: alg.TicTacToeChangePerspective
+  }
+  const exampleNode = {
+    state: [[0,1,0],[0,-1,0],[0,0,0]],
+    actionTaken: 1,
+    value: 0,
+    visits: 0,
+    expandableMoves: [1,0,1, 1,0,1, 1,1,1],
+    children: []
+  }
+  const exampleExpandedNode = {
+    state: [[-1,1,-1],[1,-1,1],[1,-1,0]],
+    actionTaken: 1,
+    value: 0,
+    visits: 0,
+    expandableMoves: [0,0,0, 0,0,0, 0,0,0],
+    children: [
+      {
+        state: [[1,-1,1],[-1,1,-1],[-1,1,-1]],
+        actionTaken: 8,
+        value: 0,
+        visits: 0,
+        expandableMoves: [0,0,0, 0,0,0, 0,0,0],
+        children: []
+      }
+    ]
+  }
+    describe('Funciones auxiliares', function() {
+      it('MCExpandNode Debe retornar el nodo expandido', function() {
+        /*
+        Ejemplo de cómo puede quedar el nodo después de expandir:
+       {
+          state: [[0,1,0],[0,-1,0],[0,0,0]],
+          actionTaken: 1,
+          value: 0,
+          visits: 0,
+          expandableMoves: [0,0,1, 1,0,1, 1,1,1],
+          children: [
+                        {
+                          state: [[-1,-1,0],[0,1,0],[0,0,0]],
+                          actionTaken: 0,
+                          value: 0,
+                          visits: 0,
+                          expandableMoves: [0,0,1, 1,0,1, 1,1,1],
+                          children: []
+                      }
+          ]
+      }
+         */
+        
+        let exampleNodeCopy = structuredClone(exampleNode);
+        alg.MCExpandNode(game)(exampleNodeCopy);
+        //console.log(exampleNodeCopy);
+        // Crea un nodo hijo
+        expect(exampleNodeCopy.children.length).toBe(1);
+        // El nodo padre tiene un expandableMoves menos 
+        expect(exampleNodeCopy.expandableMoves.filter(m => m===1).length).toBe(6);
+        // El nodo hijo tiene expandableMoves y hay uno menos que en el padre
+        expect(exampleNodeCopy.children[0].expandableMoves.filter(m => m===1).length).toBe(6);
+        // El nodo hijo tiene state y está invertido respecto al padre
+        expect(exampleNodeCopy.children[0].state[0][1]).toBe(-1);
+        // En la posición donde se ha elegido hacer el movimiento hay un -1, que es el 1 invertido
+        expect(exampleNodeCopy.children[0].state.flat()[exampleNodeCopy.children[0].actionTaken]).toBe(-1);
+        });
+   
+  
+
+    it('MCIsFullExpandedNode Debe retornar si está totalmente expandido', function() {
+      expect(alg.MCIsFullExpandedNode(exampleNode)).toBe(false);
+      expect(alg.MCIsFullExpandedNode(exampleExpandedNode)).toBe(true);
+      expect(alg.MCIsFullExpandedNode(exampleExpandedNode.children[0])).toBe(false);
+      
+    });
+ 
+  });
+   
+   });
+
+   
---
+++ src/algoritmos.js
@@ -0,0 +1,236 @@
+export {TicTacToeGetInitialState, 
+    TicTacToeGetNextState, 
+    TicTacToeGetValidMoves, 
+    TicTacToeCheckWin,
+    TicTacToeGetWinAndTerminated,
+    TicTacToeChangePlayer,
+    TicTacToeChangePerspective,
+    MCExpandNode, MCIsFullExpandedNode
+}
+
+/**
+ * Estos ejercicios son para practicar algoritmos y estructuras de datos con Javascript.
+ * La resolución de muchos de ellos no requiere más que conocimientos básicos del lenguaje.
+ * Pueden ser útiles para aprender o repasar fundamentos de programación.
+ */
+
+
+/* Bloque 1: El juego del 3 en raya */
+
+function TicTacToeGetInitialState(){
+    // Esta función retorna un array de 3x3 lleno de 0s
+    return [[0,0,0],[0,0,0],[0,0,0]]
+}
+
+function TicTacToeGetNextState(state,action,player){
+    /*
+    Esta función recibe: 
+    - Un estado en forma de matriz 3x3
+    - Una acción que es un entero de 0 a 9, que indica a qué posición se moverá el player
+    - Un player que es un número 1 o -1
+    Debe retornar una copia del estado donde se ha puesto el player
+    en el lugar donde indica action
+     */
+    let state_copy = structuredClone(state);
+    state_copy[Math.floor(action/3)][action%3] = player;
+    return state_copy;
+}
+
+function TicTacToeGetValidMoves(state){
+    /*
+    Esta función retorna un array con las "actions" o movimientos válidos
+    Un movimiento válido es la posición donde el tablero tiene un 0
+    El resultado es un array de 1s y 0s donde un 1 significa que el movimiento de esa
+    posición del array es válido. 
+    En este resultado: [0,0,0 ,1,0,0, 1,0,1] significa que se puede poner ficha en
+    las posiciones 3,6,8 del tablero. 
+    */
+    return state.flat().map(move => move === 0 ? 1 : 0)
+}
+
+function TicTacToeCheckWin(state,action){
+    /*
+    Esta función retorna true o false dependiendo si, con el action,
+    el jugador actual ha ganado.
+    En el estado que se le pasa ya se ha realizado el action
+    */
+    const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [6, 4, 2], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+    let player = state[Math.floor(action/3)][action%3];
+    let s = state.flat();
+    return winCombos.some((combo) => [s[combo[0]], s[combo[1]], s[combo[2]]].every((v) => v === player))
+   
+}
+
+function TicTacToeGetWinAndTerminated(state,action){
+    /**
+     * Esta función recibe un estado y la última acción realizada
+     * Devuelve un objecto que tendrá esta estructura:
+     * {win: boolean, terminated: boolean}
+     * Esta indicará si ha ganado y si la partida a acabado
+     * Una partida acaba si alguien gana o en tablas, es decir, que 
+     * ya no quedan jugadas válidas pero no ha ganado nadie.
+     */
+    if (TicTacToeCheckWin(state,action)){
+        return {win: true, terminated: true}
+    }
+    if (TicTacToeGetValidMoves(state).every(m => m==0)){
+        return {win: false, terminated: true}
+    }
+    return {win: false, terminated: false}
+}
+
+function TicTacToeChangePlayer(player){
+    // Retorna el otro jugador, si es 1 retorna -1 i viceversa
+    return -player;
+}
+
+function TicTacToeChangePerspective(state){
+    /*
+    Esta función sirve para invertir el juego. 
+    En el caso de ese juego, debe poner -1 donde pone 1 y 1 donde pone -1
+    OJO, que en JS existe el -0
+    */
+    return state.map(row => row.map(col => col * -1 || 0))
+}
+
+
+/* Bloque 2 El algoritmo Montecarlo Tree Search */
+
+/*
+https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
+
+El algoritmo de Montecarlo se usa para implementar una cierta
+inteligencia en la búsqueda de la mejor decisión. Se suele usar en juegos 
+tipo el 3 en raya o incluso ajedrez para encontrar la jugada con más opciones de ganar
+
+El 3 en raya tiene exactamente 303 situaciones distintas del tablero sin contar 
+las situaciones finales en las que gana alguien o hay tablas ni las
+rotaciones sobre una misma situación que son equivalentes. Si contamos todas
+las posibles situaciones son 19683. Esto es una cantidad aceptable y por eso el 3 en raya
+se considera un juego "resuelto", ya que un jugador perfecto siempre gana o empata.
+
+Pero para aprender algoritmos es ideal, ya que las reglas son simples.
+
+Vamos a implementar un algortimo de Montecarlo genérico que luego
+se podrá usar para implementar la IA del 3 en raya. 
+
+Puesto que nuestro enfoque es de Programación Funcional y queremos poder
+testar de forma muy granular, vamos a hacer funciones pequeñas que luego usaremos 
+para el algoritmo definitivo.  
+*/
+
+/* Montecarlo Tree Search utiliza un árbol. En JS se pueden hacer árboles
+con objetos literales. Por lo que vamos a usar un objeto con esta sintaxis recursiva:
+{
+    state: GameState,
+    actionTaken: number
+    value: number, 
+    visits: number,
+    expandableMoves: [] 
+    children: [
+        {value: number, visits: number, children:[...]},
+        {value: number, visits: number, children:[...]}
+    ,...]}
+
+
+Para hacerlo genérico, declararemos un objeto "game" que adaptará las funciones de un juego a este algoritmo:
+const game = {
+    getValidMoves: TicTacToeGetValidMoves,
+    getInitialState: TicTacToeGetInitialState,
+    
+}
+Esto se hará cuando se quiera usar el algoritmo.
+*/
+
+const game = {
+    getValidMoves: TicTacToeGetValidMoves,
+    getInitialState: TicTacToeGetInitialState,
+    getNextState: TicTacToeGetNextState,
+    changePerspective: TicTacToeChangePerspective
+}
+
+const MCExpandNode = (game)=> (node)=> {
+    /*
+    Un nodo tendrá un atributo expandableMoves, que tiene la lista
+    de movimientos válidos que quedan por explorar. 
+    La lista es un array de 1s y 0s donde un 1 significa que es válido y que es expandible.
+    Se debe seleccionar uno de ellos, crear un nodo hijo y añadirlo a la lista de children
+    del nodo. 
+    El nodo hijo recibirá el estado modificado por el movimiento, un valor y contador de visitas a 0
+    y la lista de movimientos válidos a partir de ahí, además de una lista vacía de nodos hijos. 
+    El estado del nodo hijo será calculado como el estado del padre con el movimiento escogido al azar de
+    entre los válidos. Además, el nodo hijo tendrá el estado con la perspectiva invertida, ya que cambia el jugador
+    Para simplificar, en esta función, vamos a mutar el node en vez de retornar una copia. 
+    */
+    let possibleMoves = node.expandableMoves.reduce((eM,current,index)=>{ 
+        if(current === 1) { eM.push(index)}
+        return eM;
+     },[]);
+    let selectedMove = possibleMoves[Math.floor(Math.random()*possibleMoves.length)];
+    let children = {
+        value: 0, 
+        actionTaken: selectedMove,
+        visits: 0, 
+        children: []
+    }
+    // Anulamos ya ese movimiento como posible expansión
+    node.expandableMoves[selectedMove] =0;
+    // Copiamos el estado anterior
+    children.state = structuredClone(node.state);
+    // Ponemos un 1 en el action porque siempre está desde la perspectiva del 1
+    children.state = game.getNextState(children.state,selectedMove,1);
+    // Cambiamos de nuevo la perspectiva para que siempre sea la del 1
+    children.state = game.changePerspective(children.state);
+    // Calculamos los movimientos expandibles del nodo hijo
+    children.expandableMoves = game.getValidMoves(children.state);
+    // Añadimos el nodo hijo
+    node.children.push(children)
+}
+
+
+const  MCIsFullExpandedNode = (node)=>{
+    /*
+    Un nodo se considera totalmente expandido 
+    si no hay más movimientos válidos y tiene al menos un nodo hijo
+    Esta función retorna true o false si esto pasa. 
+    */
+   return node.expandableMoves.every(m=> m===0) && node.children.length > 0;
+}
+
+const MCGetUcb = (node) => {
+    
+}
+
+
+function MCTSelectBestNode(game, root){
+    /*
+    Para seleccionar el mejor nodo, hay que elegir recursivamente
+    el mejor de los hijos de un nodo. De esta manera, el mejor elige su mejor hijo
+    y así sucesivamente hasta llegar a un nodo que tiene menos de 2 hijos
+    El mejor es el que tenga mejor ucb, que se calcula con la función MCGetUcb.
+    
+    Esta función recibe un nodo que actúa como raíz y se llama a sí misma para obtener el mejor
+    nodo hijo. 
+    */
+   if (MCIsFullExpandedNode(game,root)){
+    return root;
+   }
+    let ucbList = root.children.map(MCGetUcb);
+    let bestUCB = Math.max(...ucbList);
+    let bestChild = root.children[ucbList.findIndex(ucb => ucb === bestUCB)];
+    return MCTSelectBestNode[bestChild];
+    
+}
+
+function MCTSearch(game, state, numSearches){
+    let root = {
+        state: game.getInitialState(),
+        value: 0, 
+        visits: 0, 
+        expandableMoves: [game.getValidMoves(game.getInitialState())],
+        children: []};
+    for (let search of numSearches){
+        let node = MCTSelectBestNode(root)
+    }
+}
+
